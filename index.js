f417a604f72526814ded24dad62a80166d70238f
animashaun87 <animashaunbidemi07@gmail.com>
2020-05-12 22:47:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require("graphql");
+const app = express();
+
+const authors = [
+  { id: 1, name: "Pro Wole Soyinka" },
+  { id: 2, name: "Chinua Achebe" },
+  { id: 3, name: "Chimamanda Ngozi Adichie" },
+];
+
+const books = [
+  { id: 1, name: "The climate of fear", authorId: 1 },
+  { id: 2, name: "The past must address its present", authorId: 1 },
+  { id: 3, name: "Season of Anomy", authorId: 1 },
+  { id: 4, name: "Things fall apart", authorId: 2 },
+  { id: 5, name: "Arrow of God", authorId: 2 },
+  { id: 6, name: "There was a country", authorId: 2 },
+  { id: 7, name: "Purple Hibiscus", authorId: 3 },
+  { id: 8, name: "The Shivering", authorId: 3 },
+  { id: 9, name: "You in America", authorId: 3 },
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represents a book written by an author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: (book) => {
+        return authors.find((author) => author.id === book.authorId);
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This represents a author of a book",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author) => {
+        return books.filter((book) => book.authorId === author.id);
+      },
+    },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    book: {
+      type: BookType,
+      description: "A Single Book",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) => books.find((book) => book.id === args.id),
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      description: "List of All Books",
+      resolve: () => books,
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: "List of All Authors",
+      resolve: () => authors,
+    },
+    author: {
+      type: AuthorType,
+      description: "A Single Author",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) =>
+        authors.find((author) => author.id === args.id),
+    },
+  }),
+});
+
+const RootMutationType = new GraphQLObjectType({
+  name: "Mutation",
+  description: "Root Mutation",
+  fields: () => ({
+    addBook: {
+      type: BookType,
+      description: "Add a book",
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: (parent, args) => {
+        const book = {
+          id: books.length + 1,
+          name: args.name,
+          authorId: args.authorId,
+        };
+        books.push(book);
+        return book;
+      },
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: "Add an author",
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        const author = {
+          id: authors.length + 1,
+          name: args.name,
+        };
+        authors.push(author);
+        return author;
+      },
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+  mutation: RootMutationType,
+});
+
+// const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: "HelloWorld",
+//     fields: () => ({
+//       message: {
+//         type: graphQLString,
+//         resolve: () => "Hello World",
+//       },
+//     }),
+//   }),
+// });
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true,
+  })
+);
+app.listen(3003, () => console.log("Server running"));
